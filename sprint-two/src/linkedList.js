b87ae582d263dd2a10a7c91d448be0bb1a4fb4c2
Grisanu Naing <mgrisanu@gmail.com>
2016-04-20 22:37:33 -0700

    1) Working on _.addToTail => first testing run

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,25 +1,57 @@
+// functional style?
 var LinkedList = function() {
   var list = {};
+
+  // contains Node class objects with numeric keys
+  list.storage = {};
+
+  // refer to appropiate node
   list.head = null;
   list.tail = null;
 
+  // next numeric key to add new Node to
+  list.counter = 0;
+
   list.addToTail = function(value) {
+    var newNode = Node();
+    newNode.value = value;
+
+    // add new node to storage
+    this.storage[this.counter] = newNode;
+
+    if (this.head === null) { // first element in linked list
+      this.head = this.storage[this.counter];
+      this.tail = this.head;
+    } else { // NOT first element in linked list
+      // point to tail node to new tail node key
+      this.storage[this.tail].next = this.storage[this.counter];
+      // changing tail ref to new tail ref
+      this.tail = this.storage[this.counter];
+    }
+
+    this.counter++;
   };
 
   list.removeHead = function() {
+    // if (list.tail === null) { // only 1 element in linked list
+    //   list.head = null;
+    // } else {
+    //   list.head = list.head.next;
+    // }
   };
 
-  list.contains = function(target) {
+  list.contains = function(target) { 
+    // while ()
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function(value) { // each node class only points to its tail
   var node = {};
 
   node.value = value;
-  node.next = null;
+  node.next = null; // only created / assigned when call addToTail
 
   return node;
 };
@@ -27,3 +59,8 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// list empty object => with .head, .tail,
+// .addToTail(), .removeHead(), .contain()
+var aa = LinkedList();
+aa.addToTail(5)
