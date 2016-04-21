be8a356263e80cd4b60cb6ef260b89e2222cbf4f
Grisanu Naing <mgrisanu@gmail.com>
2016-04-21 11:36:32 -0700

    1) finish _.addChild method 2) attempted _.contains method=> has a bug, returning false when should return true

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,40 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  // contains an array of sub-trees
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
 var treeMethods = {};
 
+// add a value as child => sets as target of a node
 treeMethods.addChild = function(value) {
+	this.children.push(Tree(value));
 };
 
+// take any value => check a target node and all descendents if contains value
 treeMethods.contains = function(target) {
+	debugger;
+	var hasTarget = false;
+
+	// trarget in current node
+	if (this.value === target) {
+		return hasTarget || true;
+	}
+	// target not in current node
+	if (this.children.length > 0) { // target has 0 children
+		// debugger;
+		return hasTarget || false;
+	} else {
+		for (var child = 0; child < this.children.length; child++) {
+			// debugger;
+			return hasTarget || this.children[child].contains(target);
+		}
+	}
 };
 
 
